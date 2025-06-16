3dbc8efd19b854b9f2b4607890e29a0f8913774a
Jchips <20979096+Jchips@users.noreply.github.com>
2025-06-16 11:04:13 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Tab manager for Chrome
+
+A tab manager extension for Chrome browser.
+
+## Author: Jelani R
+
+## Architecture
+
+- Built with React + Vite
+- Languages used: JavaScript
+
+## Resources
+
+- [developer.chrome.com](https://developer.chrome.com/docs/extensions/get-started/tutorial/hello-world)
+- [Icon from icons8.com](https://icons8.com/)
---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+/* global chrome */
+
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [status, setStatus] = useState('');
+  const [groups, setGroups] = useState([]);
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    chrome.storage.local.get('tabGroups', (result) => {
+      setGroups(result.tabGroups || []);
+    });
+  }, []);
+
+  // const saveCurrentTabs = async () => {
+  //   const tabs = await chrome.tabs.query({ currentWindow: true });
+  //   const urls = tabs.map((tab) => tab.url); // creates array of tab urls
+  //   chrome.tabs.group({urls}, (groupId) => {
+  //     chrome.tabGroups.update(groupId, {
+  //       name: 'New group'
+  //     })
+  //   })
+  //   // chrome.storage.local.set({ savedTabs: urls }, () => {
+  //   //   setStatus('Current tabs saved');
+  //   // });
+  // };
+
+  const saveGroup = async () => {
+    const tabs = await chrome.tabs.query({ currentWindow: true });
+    const newGroup = {
+      name,
+      tabs: tabs.map((tab) => ({
+        // create object for each tab in group
+        url: tab.url,
+        title: tab.title,
+      })),
+    };
+    const updatedGroups = [...groups, newGroup];
+    chrome.storage.local.set({ tabGroups: updatedGroups }, () => {
+      setGroups(updatedGroups);
+      setName('');
+      setStatus('Group saved');
+    });
+  };
+
+  const deleteGroup = (i) => {
+    const groupsCopy = [...groups];
+    groupsCopy.splice(i, 1);
+    chrome.storage.local.set({ tabGroups: groupsCopy }, () => {
+      setGroups(groupsCopy);
+      setStatus('Deleted group');
+    });
+  };
+
+  const openGroup = (group) => {
+    if (group.tabs && group.tabs.length > 0) {
+      group.tabs.forEach((tab) =>
+        chrome.tabs.create({
+          url: tab.url,
+        })
+      );
+      setStatus('Opened tab group');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Tabs</h1>
+      <input
+        placeholder='Group Name'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={saveGroup}>Save group</button>
+
+      <h3>Saved groups</h3>
+      {groups.map((group, i) => (
+        <div key={i}>
+          <strong>{group.name}</strong>
+          <button onClick={() => openGroup(group)}>Open</button>
+          <button onClick={() => deleteGroup(i)}>Delete</button>
+        </div>
+      ))}
+
+      <p>{status}</p>
+    </div>
+  );
+}
+
+export default App;
